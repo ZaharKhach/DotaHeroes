d55cbd78a41254b20c01b17f351abbede48d9530
dendywool <zaharkhachaturyan@gmail.com>
2024-03-07 13:04:22 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
     return (
-        <Router>
             <Routes>
                 <Route exact path="/" element={<HomePage />}></Route>
                 <Route exact path="/heroes" element={<HeroesPage />}></Route>
                 <Route path="/heroes/:id" element={<SingleHeroPage/>}></Route>
             </Routes>
-        </Router>
     )
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
+import { BrowserRouter } from 'react-router-dom';
+
 import App from "./App";
 import store from "./store";
 
@@ -9,7 +11,10 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <BrowserRouter basename="/DotaHeroes">
+
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
